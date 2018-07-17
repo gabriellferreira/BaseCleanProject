9129cd427ba31c31870ccd744170163bd80f3353
Gabriel Ferreira <gabriel.lucas@dextra-sw.com>
2018-07-17 15:43:59 -0300

    basefragment update

--- app/src/main/java/br/com/gabrielferreira/baseclean/presentation/view/fragment/BaseFragment.kt
+++ app/src/main/java/br/com/gabrielferreira/baseclean/presentation/view/fragment/BaseFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import br.com.gabrielferreira.baseclean.presentation.internal.di.AppApplication
+import br.com.gabrielferreira.baseclean.presentation.internal.di.ControllerComponent
+import br.com.gabrielferreira.baseclean.presentation.internal.di.ControllerModule
 import br.com.gabrielferreira.baseclean.presentation.view.BaseContract
 
 abstract class BaseFragment<T, in V> : Fragment(), BaseContract.View where T : BaseContract.Presenter<V>, V : BaseContract.View {
+
+    private var mControllerComponent: ControllerComponent? = null
 
     protected var presenter: T? = null
         set(value) {
@@ -18,46 +25,29 @@ abstract class BaseFragment<T, in V> : Fragment(), BaseContract.View where T : B
 
     protected abstract fun createPresenter(): T
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        presenter = createPresenter()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        safeAttachView()
-    }
-
     @Suppress("UNCHECKED_CAST", "TooGenericExceptionThrown")
-    private fun safeAttachView() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        presenter = this.createPresenter()
         try {
             presenter?.attachView(this as V)
         } catch (e: ClassCastException) {
             throw RuntimeException("Type V must be the same type of this class", e)
         }
-    }
-
-    private fun setupToolbar(toolbar: Toolbar) {
-        activity?.let {
-            if (it is AppCompatActivity) {
-                it.setSupportActionBar(toolbar)
-
-                it.supportActionBar?.let { actionBar ->
-                    actionBar.title = toolbar.title
-                }
-            }
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        if (presenter != null) {
-            presenter?.detachView()
-        }
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        presenter?.onDestroy()
+        presenter?.detachView()
         presenter = null
+    }
+
+    protected fun getControllerComponent(): ControllerComponent {
+        if (mControllerComponent == null) {
+            mControllerComponent = AppApplication.applicationComponent
+                    .newControllerComponent(ControllerModule(activity as AppCompatActivity))
+        }
+        return mControllerComponent!!
     }
 }
