712f4b51b9756cb96c2751452582976ca46195b6
Gabriel Ferreira <gabriel.lucas@dextra-sw.com>
2018-08-19 22:52:13 -0300

    basefragment fix and some lib update

--- app/src/main/java/br/com/gabrielferreira/baseclean/presentation/view/fragment/BaseFragment.kt
+++ app/src/main/java/br/com/gabrielferreira/baseclean/presentation/view/fragment/BaseFragment.kt
@@ -3,19 +3,16 @@ package br.com.gabrielferreira.baseclean.presentation.view.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import br.com.gabrielferreira.baseclean.presentation.internal.di.AppApplication
 import br.com.gabrielferreira.baseclean.presentation.internal.di.ControllerComponent
 import br.com.gabrielferreira.baseclean.presentation.internal.di.ControllerModule
 import br.com.gabrielferreira.baseclean.presentation.view.BaseContract
 
-abstract class BaseFragment<T, in V> : Fragment(), BaseContract.View where T : BaseContract.Presenter<V>, V : BaseContract.View {
+abstract class BaseFragment<T, in V> : Fragment(),
+        BaseContract.View where T : BaseContract.Presenter<V>, V : BaseContract.View {
 
     private var mControllerComponent: ControllerComponent? = null
-
     protected var presenter: T? = null
         set(value) {
             if (field == null) {
@@ -25,15 +22,23 @@ abstract class BaseFragment<T, in V> : Fragment(), BaseContract.View where T : B
 
     protected abstract fun createPresenter(): T
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        safeAttachView()
+    }
+
     @Suppress("UNCHECKED_CAST", "TooGenericExceptionThrown")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        presenter = this.createPresenter()
+    private fun safeAttachView() {
         try {
             presenter?.attachView(this as V)
         } catch (e: ClassCastException) {
             throw RuntimeException("Type V must be the same type of this class", e)
         }
-        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onDestroy() {
